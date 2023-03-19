f8e38fa2c458fba2113c9a5422381ba6f342956b
lstheis <115257808+lstheis@users.noreply.github.com>
2023-03-19 15:41:52 +0100

    velocities done

--- EulerSolver/State.java
+++ EulerSolver/State.java
@@ -48,9 +48,38 @@ public class State {
         //Sun first row again
 
         //Venus
-        velocities[1][] = 
-        velocities[1][] = 
-        velocities[1][] = 
+        velocities[1][0] = -34.0236737066136;
+        velocities[1][1] = -8.96521274688838;
+        velocities[1][2] = 1.84061735279188;
 
+        //Earth
+        velocities[2][0] = 5.05251577575409;
+        velocities[2][1] = -29.3926687625899;
+        velocities[2][2] = 0.00170974277401292;
+
+        //Moon
+        velocities[3][0] = 4.34032634654904;
+        velocities[3][1] = -30.0480834180741;
+        velocities[3][2] = -0.0116103535014229;
+
+        //Mars
+        velocities[4][0] = -17.6954469224752;
+        velocities[4][1] = -13.4635253412947;
+        velocities[4][2] = 0.152331928200531;
+
+        //Jupiter
+        velocities[5][0] = -4.71443059866156;
+        velocities[5][1] = 12.8555096964427;
+        velocities[5][2] = 0.0522118126939208;
+
+        //Saturn
+        velocities[6][0] = 4.46781341335014;
+        velocities[6][1] = 8.23989540475628;
+        velocities[6][2] = -0.320745376969732;
+
+        //Titan
+        velocities[7][0] = 8.99593229549645;
+        velocities[7][1] = 11.1085713608453;
+        velocities[7][2] = -2.25130986174761;
     }
 }

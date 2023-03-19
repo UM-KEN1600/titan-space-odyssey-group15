60036c6901af82b3bdd347da10abc82dd9c1f05e
lstheis <115257808+lstheis@users.noreply.github.com>
2023-03-19 15:23:08 +0100

    set up matrix with values, start on positions

--- EulerSolver/State.java
+++ EulerSolver/State.java
@@ -1,5 +1,34 @@
 package EulerSolver;
 
 public class State {
+
+    double[][] positions = new double[7][3];
+    double[][] velocities = new double[7][3];;
+
+    public State()
+    {
+        //Sun is first row :)
     
+        //Venus
+        positions[1][0] = -28216773,9426889;
+        positions[1][1] = 103994008,541512;
+        positions[1][2] = 3012326,64296788;
+
+        //Earth
+        positions[2][0] = -148186906,893642;
+        positions[2][1] = -27823158,5715694;
+        positions[2][2] = 33746,8987977113;
+
+        //Moon
+        positions[2][] = -148458048,395164
+        positions[2][] = 
+        positions[2][]
+
+        positions[3][]
+        positions[4][]
+        positions[5][]
+        positions[6][]
+
+        //velocity here
+    }
 }

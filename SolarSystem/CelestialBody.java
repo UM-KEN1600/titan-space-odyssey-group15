1ede3f26b9364af349fee9476d850bd732e86667
lstheis <115257808+lstheis@users.noreply.github.com>
2023-03-20 10:41:51 +0100

    setup method to initiate the celestial bodies

--- SolarSystem/CelestialBody.java
+++ SolarSystem/CelestialBody.java
@@ -9,23 +9,25 @@ import EulerSolver.*;
 //represents comets, asteroids, moons etc
 
 public class CelestialBody {
+    public double mass;
+    public double radius;
+    public int rowInState;
+    CelestialBody[] list = new CelestialBody[9];
 
     String name;
     Color color;
-    double mass;
-    double radius;
+
     double x1;
     double x2;
     EulerSolver.State state = new State();
-    
-    CelestialBody(String name, Color color, double mass, double radius, double x1, double x2){
+
+    CelestialBody(double mass, double radius, int rowInState, String name, Color color){
 
         this.name = name;
         this.color= color;
         this.mass = mass;
         this.radius = radius;
-        this.x1 = x1;
-        this.x2 = x2;
+        this.rowInState = rowInState;
     }
 
     public String getName() {
@@ -107,6 +109,31 @@ public class CelestialBody {
         //TO DO
     }
 
+    /**
+     * sets up the celestial bodies with their respective mass, radisu and row in the State matrix. They are then added to the list of celestial bodies
+     */
+    public void setupCelestialBodies()
+    {
+        Color tempColor = new Color(0, 0, 0);
+        CelestialBody sun = new CelestialBody(1.9885 * Math.pow(10, 30), 1, 0, "Sun", tempColor);
+        CelestialBody venus = new CelestialBody(48.685 * Math.pow(10, 23), 1, 1, "Venus", tempColor);
+        CelestialBody earth = new CelestialBody(5.97219 * Math.pow(10, 24), 6370, 2, "Earth", tempColor);
+        CelestialBody moon = new CelestialBody(7.349* Math.pow(10, 22), 1, 3, "Moon", tempColor);
+        CelestialBody mars = new CelestialBody(6.4171* Math.pow(10, 23), 1, 4, "Mars", tempColor);
+        CelestialBody jupiter = new CelestialBody(189818722* Math.pow(10, 19), 1, 5, "Jupiter", tempColor);
+        CelestialBody saturn = new CelestialBody(5.6834* Math.pow(10, 26), 1, 6, "Saturn", tempColor);
+        CelestialBody titan = new CelestialBody(13455.3* Math.pow(10, 19), 2575, 7, "Titan", tempColor);
+        CelestialBody spaceship = new CelestialBody(50000, 1, 8, "Spaceship", tempColor);
 
+        list[0] = sun;
+        list[1] = venus;
+        list[2] = earth;
+        list[3] = moon;
+        list[4] = mars;
+        list[5] = jupiter;
+        list[6] = saturn;
+        list[7] = titan;
+        list[8] = spaceship;
+    }
 
 }

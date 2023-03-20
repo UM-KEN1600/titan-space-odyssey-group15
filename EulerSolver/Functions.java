c31d2ce9f28c7fcea6c7d929620e303391e9d33a
lstheis <115257808+lstheis@users.noreply.github.com>
2023-03-20 13:30:39 +0100

    functions

--- EulerSolver/Functions.java
+++ EulerSolver/Functions.java
@@ -8,6 +8,10 @@ import SolarSystem.CelestialBody;
 public class Functions {
 
     double G = 6.6743 * Math.pow(10, -20);
+
+    CelestialBody listOfCelestialBodies[] = CelestialBody.list;
+
+    int rowSpaceship = 8;
     
     /**
      * calculates the force of one celestial body on another celestial body
@@ -35,15 +39,59 @@ public class Functions {
      * @param spaceship
      * @return sum of all forces on the spaceship
      */
-    public double[] forceOnSpaceship(CelestialBody[] celestialBodies, CelestialBody spaceship){
+    public double[] forceOnSpaceship(CelestialBody spaceship){
         
         double[] fullForce = new double[3];
 
-        for(CelestialBody planet : celestialBodies)
+        for(CelestialBody planet : listOfCelestialBodies)
         {
             fullForce = VectorOperations.vectorAddition(fullForce, forceCalculator(planet, spaceship));
         }
 
         return VectorOperations.vectorScalarMultiplication(fullForce, -1);
+    }
+
+    /**
+     * calculates the forces of other bodies on a body A, ignoring the spaceship
+     * @param bodyA
+     * @return
+     */
+    public double[] forceOnPlanet(CelestialBody bodyA)
+    {
+        double[] fullForce = new double[3];
+
+        for(int i = 0; i < 8; i++) //leaves out spaceship
+        {
+            if(i != bodyA.rowInState)
+            {
+                fullForce = VectorOperations.vectorAddition(fullForce, forceCalculator(listOfCelestialBodies[i], listOfCelestialBodies[bodyA.rowInState]));
+            }
+            
+        }
+
+        return VectorOperations.vectorScalarMultiplication(fullForce, -1);
+    }
+
+    /**
+     * calculates the velocity of the spaceship at a certain time
+     * @param time
+     * @param spaceShip
+     * @return
+     */
+    public double[] velocityOfSpaceship(double time, CelestialBody spaceShip)
+    {
+        double[] velocity = new double[3];
+
+        //velocity1 = velocity0 + acceleration * time
+        velocity = VectorOperations.vectorScalarMultiplication(VectorOperations.vectorAddition(State.velocities[spaceShip.rowInState], VectorOperations.vectorScalarMultiplication(forceOnSpaceship(spaceShip), spaceShip.mass)), time);
+
+        return velocity;
+    }
+
+    public double[] newPositionOfSpaceship(double time, CelestialBody spaceShip)
+    {
+        double[] position = new double[3];
+
+        position = VectorOperations.vectorAddition(position, position) VectorOperations.vectorScalarMultiplication(State.velocities[spaceShip.rowInState], time);
     }
 }

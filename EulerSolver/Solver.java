b1e967227052b5f4a1aae96342bc41ea857e7b2e
Estebanix29 <115260182+Estebanix29@users.noreply.github.com>
2023-03-22 11:46:30 +0100

    Implemented "Euler" Solver
    dedicated position and velocity calculations have been moved to a separate class and simulation has been modified so it uses it

--- EulerSolver/Solver.java
+++ EulerSolver/Solver.java
@@ -10,46 +10,48 @@ import SolarSystem.Spaceship;
 
 public class Solver {
     // need initial values for x and y, coordinates I guess
-    public static double[] currentPosition = new double[3];
-    public static double[] nextPosition = new double[3];
-    public static double[] currentVelocity = new double[3];
+   
     public static double[] nextVelocity = new double[3];
-    public static double h = 0.05;
-    public static double[] acc = new double[3];
+    
 
     public static void main(String[] args) {
-        currentPosition[0] = State.positions[11][0];
-        currentPosition[1] = State.positions[11][1];
-        currentPosition[2] = State.positions[11][2];
-        acc[0] = 0;
-        acc[1] = 60;
-        acc[2] = 60;
+        
+      
         
     }
 
 
     public static double[] accelerationCalculation(double mass, double[] force) {
-        acc = VectorOperations.vectorScalarDivision(force, mass);
-        System.out.println(acc);
-        return acc;
+        double[] acceleration = new double[3];
+        acceleration = VectorOperations.vectorScalarDivision(force, mass);
+        System.out.println(acceleration);
+        return acceleration;
     
     }
 
-    public static double[] nextPosition(double[] currentPosition, double[] currentVelocity, double h) {
-        nextPosition = VectorOperations.vectorAddition(currentPosition, VectorOperations.vectorScalarMultiplication(currentVelocity, h));
+    public static double[] nextPosition(double[] currentPosition, double[] currentVelocity, double timestep) {
+        double[] nextPosition = new double[3];
+        nextPosition = VectorOperations.vectorAddition(currentPosition, VectorOperations.vectorScalarMultiplication(currentVelocity, timestep));
         currentPosition = nextPosition;
         System.out.println(currentPosition);
         return currentPosition;
     }
 
-    public static double[] nextVelocity(double[] currentVelocity, double[] accerleration){
-        nextVelocity = VectorOperations.vectorAddition(currentVelocity, VectorOperations.vectorScalarMultiplication(accerleration, h)); 
+    public static double[] nextVelocity(double[] currentVelocity, double[] acceleration, double timestep){
+        double[] nextVelocity = new double[3];
+        nextVelocity = VectorOperations.vectorAddition(currentVelocity, VectorOperations.vectorScalarMultiplication(acceleration, timestep)); 
         currentVelocity = nextVelocity;
+        
         System.out.println(currentVelocity);
         return currentVelocity; 
     }
 
-    
+    public static double[] solve(CelestialBody body, double timestep){
+        double[] acceleration = accelerationCalculation(body.getMass(), State.getForce(body.rowInState));
+        double[] velocity = nextVelocity(State.getVelocity(body.rowInState), acceleration, timestep);
+        State.setVelocity(body.rowInState, velocity);
+        return (nextPosition(State.getPosition(body.rowInState), velocity, timestep));
+    }
 
 
 }

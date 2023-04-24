5cd09fe2727d0d583ef59aa80774327a32beca7b
Estebanix29 <115260182+Estebanix29@users.noreply.github.com>
2023-04-24 14:50:40 +0200

    fixes to impulse
    i fucked up with the calculations whoops.
    Now the additional velocity vector needed is calculated correctly.

--- PhysicsEngine/Thrust.java
+++ PhysicsEngine/Thrust.java
@@ -5,12 +5,19 @@ public class Thrust {
 
     //Method to calculate the impulse in the timestep
     //Takes in an array of forces (a force vector) and the timestep being used
-    public double impulse(double[] forceVector, double timestep){
+    public double[] impulse(double[] forceVector, double timestep){
         //does a check if the total force is bigger or not than the max force possible 
         if (VectorOperations.magnitude(forceVector[0], forceVector[1], forceVector[2]) > (3*10^7)){
-            return -1;
+            return null;
         }
-        double impulse = (3*10^7)*timestep;
-        return impulse/50000; //returns how much velocity has to be added 
+        
+        double[] impulse = new double[3];
+        impulse[0] = (forceVector[0] * timestep)/50000;
+        impulse[1] = (forceVector[1] * timestep)/50000;
+        impulse[2] = (forceVector[2] * timestep)/50000;
+
+        return impulse; //returns how much velocity has to be added 
     }
+
+    public double fuelConsumption()
 }

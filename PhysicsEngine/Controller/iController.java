d732eac87c7e8c8ee7a801c34f3ed6eee680b161
AlexaVT03 <an.vanthiel@student.maastrichtuniversity.nl>
2023-06-12 18:34:15 +0200

    fixed the errors :)

--- PhysicsEngine/Controller/iController.java
+++ PhysicsEngine/Controller/iController.java
@@ -1,5 +1,5 @@
 package PhysicsEngine.Controller;
 
 public interface iController {
-    public double[] getNextPositon();
+    public double[] getNextPosition(double[] currentVelocity, double[] currentPosition, double u, double v, double theta);
 }

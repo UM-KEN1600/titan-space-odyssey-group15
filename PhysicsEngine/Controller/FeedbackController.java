317e3a389cd4171e0c547e13973c2adbf89f7e36
Estebanix29 <115260182+Estebanix29@users.noreply.github.com>
2023-06-12 14:15:37 +0200

    final values added

--- PhysicsEngine/Controller/FeedbackController.java
+++ PhysicsEngine/Controller/FeedbackController.java
@@ -2,6 +2,14 @@ package PhysicsEngine.Controller;
 
 public class FeedbackController implements iController{
 
+    //Final Values needed
+    //NOTE: Values are in km, not m. (Apart from angle)
+    final double xFINAL = 0.0001;
+    final double xVelocityFINAL = 0.0001;
+    final double yVelocityFINAL = 0.0001;
+    final double angleFINAL = 0.02;
+    final double angularVelocityFINAL = 0.01;
+
     @Override
     public double[] getNextPosition(double[] currentVelocity, double[] currentPosition, double u, double v, double theta) {
         return new double[0];

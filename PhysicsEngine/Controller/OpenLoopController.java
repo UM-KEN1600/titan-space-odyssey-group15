d329f7fc0b9eb4b5b9d44b95e93bd3c1f6ce9d84
lstheis <115257808+lstheis@users.noreply.github.com>
2023-06-13 10:57:35 +0200

    equations implemented and copied the constants

--- PhysicsEngine/Controller/OpenLoopController.java
+++ PhysicsEngine/Controller/OpenLoopController.java
@@ -1,8 +1,43 @@
 package PhysicsEngine.Controller;
 
 public class OpenLoopController implements iController{
+    //Final Values needed
+    //NOTE: Values are in km, not m. (Apart from angle)
+    final double xFINAL = 0.0001;
+    final double xVelocityFINAL = 0.0001;
+    final double yVelocityFINAL = 0.0001;
+    final double thetaFINAL = 0.02;
+    final double angularVelocityFINAL = 0.01;
+
+    //Max constraints for some values
+    final double g = 0.001352;
+    final double maxThrust = 10*g;
+    final double maxTorque = 1;
+
+
+    //Current Values of the probe
+    public double[] currentPosition;
+    public double[] currentVelocity;
+
+    //Timestep being used in the current instance
+    public double timestep;
+    
+    //Angle that will be used in the calculations that moment
+    public double theta;
+
+
     @Override
     public double[][] getNextState(double[] currentVelocity, double[] currentPosition, double u, double v, double theta) {
         return new double[0][0];
+    }
+
+    private double calculateXAcceleration(double u, double theta)
+    {
+        return u * Math.sin(theta);
+    }
+
+    private double calculateYAcceleration(double u, double theta)
+    {
+        return u * Math.cos(theta) - g;
     }
 }

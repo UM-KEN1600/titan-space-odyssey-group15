1627d5b0453c81106176b9aeae2246c6be0bafb4
Michael-30-creator <mikihempel30@gmail.com>
2023-06-14 11:21:02 +0200

    Update Simulation.java

--- PhysicsEngine/Simulations/Simulation.java
+++ PhysicsEngine/Simulations/Simulation.java
@@ -10,7 +10,7 @@ import SolarSystem.CelestialBody;
 
 public class Simulation {
 
-    final boolean SHOWENDPOSITIONS = true;
+    final boolean SHOWENDPOSITIONS = false;
 
     Functions functions = new Functions();
     iSolver solver;
@@ -21,7 +21,7 @@ public class Simulation {
     int lengthOfSimulation = 31536000 * 2; //seconds in a year //
 
     //These are the velocities that have to be changed to modify the probe at the beginning or at the point to go back
-    double[] startingVelocity = {49.58313440693111, 38.29506290304066, 1.9666588900013093};
+    double[] startingVelocity = {49.58306860517117, 38.2950835525803, 1.9666795395409593};
     double[] wayBackVelocity = {-42.96570036902944, 20.2335581826131, 1.5975552915589475};
 
     //Both of these are used to change the phases of the mission
@@ -80,6 +80,8 @@ public class Simulation {
 
             System.out.println("Probe:");
             System.out.println(Arrays.toString(probePosition));
+            System.out.println("Probe velocity entering titan:");
+            System.out.println(Arrays.toString(state.getState()[8][1]));
             System.out.println("Titan:");
             System.out.println(Arrays.toString(titanPosition));
             System.out.println("distance:");
@@ -93,7 +95,7 @@ public class Simulation {
      * returns the actual distance from Titan to the probe
      */
     private double getDistaceProbeTitan(){
-        return VectorOperations.euclideanForm(state.getState()[8][0],state.getState()[7][0]);
+        return VectorOperations.euclideanForm(state.getState()[8][0],state.getState()[7][0])- CelestialBody.bodyList[7].radius;
     }
 
     double timeInOrbit = 10200; //seconds, derived from calculations of ORBITAL TIME with respect to Titan's mass and the spaceship's distance
@@ -104,7 +106,7 @@ public class Simulation {
      */
     private void orbit()
     {
-        if(getDistaceProbeTitan() < (300 + CelestialBody.bodyList[7].radius) && goIntoOrbit == true)
+        if(getDistaceProbeTitan() < (300) && goIntoOrbit == true)
         {
             spacecraftEngine(Functions.getVelocityForOrbit(state.getState()));
             goIntoOrbit = false;
@@ -117,7 +119,7 @@ public class Simulation {
     }
 
     public boolean inOrbit(){
-        if(getDistaceProbeTitan() < (300 + CelestialBody.bodyList[7].radius)){
+        if(getDistaceProbeTitan() < (300)){
             inOrbit = true;
         }
         return inOrbit;

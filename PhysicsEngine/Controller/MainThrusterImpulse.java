501f42aa8f1cc4b0ed421eafa3863fc152f024fd
lstheis <115257808+lstheis@users.noreply.github.com>
2023-06-15 11:38:51 +0200

    calculate the change in velocity based on a thrust given

---
+++ PhysicsEngine/Controller/MainThrusterImpulse.java
@@ -0,0 +1,51 @@
+package PhysicsEngine.Controller;
+
+import java.util.Arrays;
+
+import PhysicsEngine.Operations.VectorOperations;
+
+public class MainThrusterImpulse {
+
+    double thrust;
+    double[] currentVelocity;
+
+    public MainThrusterImpulse(double thrust, double[] currentVelocity)
+    {
+        this.thrust = thrust;
+        this.currentVelocity = currentVelocity;
+    }
+
+    public double[] changeInVelocity()
+    {
+        double h = Math.sqrt(Math.pow(currentVelocity[0], 2) + Math.pow(currentVelocity[1], 2));
+        System.out.println(h);
+        h += thrust;
+        System.out.println(h);
+
+        double a = Math.cos(calculateAngle(currentVelocity, new double[]{10,0})) * h;
+
+        double b = Math.sin(calculateAngle(currentVelocity, new double[]{10,0})) * h;
+
+        double[] velocity = new double[2];
+        velocity[0] = a;
+        velocity[1] = b;
+
+        return velocity;
+    }
+
+        private double calculateAngle(double[] vectorA, double[] vectorB)
+    {
+        double dotProduct = VectorOperations.dotProduct(vectorA,vectorB);
+        double aMag = VectorOperations.magnitude(vectorA);
+        double bMag = VectorOperations.magnitude(vectorB);
+
+        double check = vectorA[0]*vectorB[1] - vectorA[0]*vectorB[0];
+        
+        if(check < 0)
+        {
+            return 2*Math.PI - Math.acos(dotProduct/(aMag * bMag));
+        }
+
+        return Math.acos(dotProduct/(aMag * bMag));
+    }
+}

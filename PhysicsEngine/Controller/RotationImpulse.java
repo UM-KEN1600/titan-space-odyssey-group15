10cac0c53935b25d08b8185ee1b7658288e50624
Sebastian Schmülling <sebastian.schmuelling@gmail.com>
2023-06-15 12:34:36 +0200

    added RotationImpulse Method

--- PhysicsEngine/Controller/RotationImpulse.java
+++ PhysicsEngine/Controller/RotationImpulse.java
@@ -5,6 +5,9 @@ public class RotationImpulse {
     private double angleOfRotation;
     private double torque;
 
+    public final double VMAX = 60;
+    public final int maxTimeRotation = 3;
+
     /**
      * A Rotation Impulse can be used to store the combination of an angle with a given torque
      * This allows the calculation of how long the rotation will take, how far it will overshoot,
@@ -18,37 +21,21 @@ public class RotationImpulse {
         this.torque = torque;
     }
 
-    public double getTimeNeededForRotation()
-    {
-        return angleOfRotation/torque;
+    private double calculateVMax(double angle, double t){
+        return angle/Math.pow(t,2);
     }
 
-    /**
-     * calculates the angle that is overshot based on the time needed to rotate, since we can only
-     * check the angle every timestep we do calculations, if we use the maxTorque, the
-     * rotation will never be perfect
-     * @return
-     */
-    private double getOvershoot()
-    {
-        return torque - angleOfRotation%torque;
+    private double calculateTimeNeeded(double targetAngle){
+
+        double intermediateAngle = targetAngle/2;
+
+        for (int t = 0; t < maxTimeRotation; t++) {
+            if(intermediateAngle/t <= VMAX){
+               return t;
+            }
+        }
+        return 0;
     }
 
-    /**
-     * rotates the overshoot back
-     * @return torque needed to correct the overshoot
-     */
-    public double correctOverShoot()
-    {
-        return getOvershoot()*-1.0;
-    }
 
-    /**
-     * stops correction of overshoot
-     * @return torque needed to stand still in wanted angle
-     */
-    public double standStill()
-    {
-        return correctOverShoot()*-1.0;
-    }
 }
--- PhysicsEngine/States/RocketState.java
+++ PhysicsEngine/States/RocketState.java
@@ -3,10 +3,10 @@ package PhysicsEngine.States;
 public class RocketState {
 
     private static RocketState instance;
-    private double[] positionsMidpoint = new double[3];
-    private double[] positionsTail = new double[3];
-    private double[] positionsTip = new double[3];
-    private double[] velocities = new double[3];
+    private double[] positionsMidpoint = new double[2];
+    private double[] positionsTail = new double[2];
+    private double[] positionsTip = new double[2];
+    private double[] velocities = new double[2];
 
     double angle;
 
@@ -72,4 +72,6 @@ public class RocketState {
     public void setAngle(double angle) {
         this.angle = angle;
     }
+
+    public void positionTranslator(double[] velocity)
 }

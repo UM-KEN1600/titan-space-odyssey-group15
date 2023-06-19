b3315af05b8b0df70225576e339889408f2f8cee
Michael-30-creator <mikihempel30@gmail.com>
2023-06-19 15:50:03 +0200

    Wind adapt the force to the layer
    Now,  Calling applyWind method with the parameters (currentVelocity, distanceProbeTitan), maxWindVelocity is adapted to the atmosphere layer. Remains the method applyWind if we don't want the adaptiveness.

--- PhysicsEngine/Wind/Wind.java
+++ PhysicsEngine/Wind/Wind.java
@@ -7,11 +7,11 @@ import java.util.Random;
  * Create an instance of Wind using LeftWind, RightWind or LeftAndRightWind classes.
  * Call the constructor with the preferred maxWindVelocity in km/h value.
  * Call the method applyWind (i.e. currentVelocity = instanceName.applyWind(currentVelocity); )
+ * Calling applyWind with the parameters (currentVelocity, distanceProbeTitan), maxWindVelocity is adapted to the atmosphere layer
 */
 public abstract class Wind {
 
-    protected double[] currentProbeVelocity;
-    protected double maxWindVelocity;
+    protected final double maxWindVelocity;
     
     /**
      * @param maxWindVelocityKmh is the maximum velocity the wind can have in km/h, it descreases getting closer titan
@@ -24,12 +24,13 @@ public abstract class Wind {
      * @param currentProbeVelocity is the velocity the wind have to be applied to
      * @return new probe velocity after the wind effect, i.e. currentVelocity = applyWind(currentVelocity)
      */
-    public abstract double[] applyWind(double[] currentProbeVelocity);// thinking about another parameter currentDistance, to adapt the maxWind 
+    public abstract double[] applyWind(double[] currentProbeVelocity);
+
+    //adapt the the wind to the altitude (lower altitude, more wind)
+    public abstract double[] applyWind(double[] currentProbeVelocity, double distanceFromSurface);
 
 
-    //to the distance from titan since near the surface the wind is lower
-
-    public double[] calculateWindVelocity(double[] angleBoundaries) {
+    public double[] calculateWindVelocity(double[] angleBoundaries, double distanceFromSurface) {
 
         Random random = new Random();
         double windChangeX = 0;// wind velocity on x
@@ -44,10 +45,37 @@ public abstract class Wind {
         //find the angle the wind arrives from
         angle = random.nextDouble(endAngle - startAngle) + startAngle;
 
-        windChangeX = maxWindVelocity * Math.cos(Math.toRadians(angle)); // radiants are expected in imput, not degrees
+        windChangeX = adaptWindToLayerOfAtmosphere(distanceFromSurface) * Math.cos(Math.toRadians(angle)); // radiants are expected in imput, not degrees
         //windChangeY = maxWindVelocity * Math.sin(Math.toRadians(angle)); // not changing it for now
 
         return new double[] {windChangeX, windChangeY};
 
     }
+
+    /**
+     * 
+     * @param currentDistanceFromSurface
+     * @return the maxWindvelocity relative to the layer of the atmosphere the probe is in and the maxVelocity bound
+     * Layers of Titan's atmosphere:
+            1. Troposphere: 0-32 km (0-20 mi)
+            2. Stratosphere: 32-100 km (20-62 mi)
+            3. Mesosphere: 100-210 km (62-130 mi)
+            4. Thermosphere: Above 210 km (130 mi)
+     */
+    private double adaptWindToLayerOfAtmosphere(double currentDistanceFromSurface){
+
+        if (currentDistanceFromSurface <= 32) {  //Troposphere
+            return 4/4 * maxWindVelocity;
+
+        } else if (currentDistanceFromSurface <= 100) { //Stratosphere
+            return 3/4 * maxWindVelocity;
+
+        } else if (currentDistanceFromSurface <= 210) { //Mesosphere
+            return 2/4 * maxWindVelocity;
+
+        } else {     //Thermosphere over 210km
+            return 1/4 * maxWindVelocity;
+        } 
+    }
+
 }

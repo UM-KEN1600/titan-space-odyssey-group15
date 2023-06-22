81ee24134a87923e027f0d34de3669fade6d10b3
Gabriel <eutu20032003@gmail.com>
2023-06-22 12:27:52 +0200

    -

---
+++ SolarSystem/LandingRunner.java
@@ -0,0 +1,46 @@
+package SolarSystem;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
+import javax.swing.JFrame;
+import javax.swing.SwingUtilities;
+
+public class LandingRunner {
+
+    public static LandingDraw landing = new LandingDraw();
+  
+    public static void run(){
+       
+        //main frame
+        JFrame mainFrame = new JFrame("Landing");
+        mainFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        mainFrame.setSize(1000, 600);
+        mainFrame.setLocationRelativeTo(null);
+        //mainFrame.setUndecorated(true);
+        mainFrame.add(landing);
+        
+        // timer to move celestial bodies
+        Timer t = new Timer();
+        TimerTask tt = new TimerTask() {
+            int a = 0;
+            @Override
+            public void run() {
+                landing.repaint();
+                if (a == 99) {
+                    t.cancel();
+                    SwingUtilities.invokeLater(new Runnable() {
+                        public void run() {
+                                                 
+                         }
+                    });
+                }
+                else if(a < 99)
+                a++;
+            }
+        };
+        t.schedule(tt, 0, 100);
+        
+        mainFrame.setVisible(true);
+}
+}
